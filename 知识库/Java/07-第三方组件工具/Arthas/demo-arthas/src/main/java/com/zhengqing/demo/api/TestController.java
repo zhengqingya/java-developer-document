e837a983ccc6855633671baa8aaac2e074dfe22f
zhengqingya <960869719@qq.com>
2022-10-02 17:08:21 +0800

    update doc

--- 知识库/Java/07-第三方组件工具/Arthas/demo-arthas/src/main/java/com/zhengqing/demo/api/TestController.java
+++ 知识库/Java/07-第三方组件工具/Arthas/demo-arthas/src/main/java/com/zhengqing/demo/api/TestController.java
@@ -32,5 +32,4 @@ public class TestController {
         return "66";
     }
 
-
 }
--- 知识库/Java/07-第三方组件工具/Arthas/demo-arthas/src/main/java/com/zhengqing/demo/api/WatchController.java
+++ 知识库/Java/07-第三方组件工具/Arthas/demo-arthas/src/main/java/com/zhengqing/demo/api/WatchController.java
@@ -1,27 +1,6 @@
-### watch
-
-> 监听方法执行情况: 返回值、抛出异常、入参
-
-```shell
-# 监听提交参数 & 返回对象值
-watch com.zhengqing.demo.api.TestController test1 "{params[0],returnObj}"
-
-# 监听第一个提交参数 & 返回对象中的name字段值
-watch com.zhengqing.demo.api.TestController test2 "{params[0],returnObj.obj.name}"
-
-# 监听第一个提交参数 & 指定输出结果的属性遍历深度(默认为1)
-watch com.zhengqing.demo.api.TestController test2 "{params[0],returnObj}" -x 3
-
-# 监听异常 -n：只执行2次
-watch com.zhengqing.demo.api.TestController test2 "{params[0],returnObj,throwExp}" -x 3 -n 2
-```
-
-![watch.png](images/watch.png)
-
-```java
 package com.zhengqing.demo.api;
 
-import cn.hutool.json.JSONUtil;
+
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
@@ -33,11 +12,20 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+
+/**
+ * <p> 测试api </p>
+ *
+ * @author zhengqingya
+ * @description
+ * @date 2022/9/04 18:30
+ */
 @Slf4j
 @RestController
 @RequestMapping("api")
 @Api(tags = "测试api")
-public class TestController {
+public class WatchController {
+
 
     /**
      * curl http://127.0.0.1:666/api/test
@@ -49,12 +37,13 @@ public class TestController {
     }
 
     /**
+     * Arthas: 监听方法的执行情况  ex: 入参、返回值
      * curl http://127.0.0.1:666/api/test1
      */
     @GetMapping("test1")
     @ApiOperation("test1")
     public User test1(String str) {
-        System.out.println("hello");
+//        log.info("请求参数：{}", str);
         return User.builder()
                 .id(1)
                 .name("zhengqingya")
@@ -68,7 +57,7 @@ public class TestController {
     @PostMapping("test2")
     @ApiOperation("test2")
     public User test2(@RequestBody Map<String, String> map) {
-        System.out.println(JSONUtil.toJsonStr(map));
+//        log.info("请求参数：{}", JSONUtil.toJsonStr(map));
         return User.builder()
                 .id(1)
                 .name("zhengqingya")
@@ -87,4 +76,3 @@ public class TestController {
     }
 
 }
-```
