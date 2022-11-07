9bbbee66906e168a2982551baacd09a9e7cc6c4b
zhengqingya <960869719@qq.com>
2022-11-07 16:15:51 +0800

    update doc

--- 知识库/前端/04-uniapp项目实战/small-tools-app/pages/product/product.vue
+++ 知识库/前端/04-uniapp项目实战/small-tools-app/pages/product/product.vue
@@ -2,20 +2,22 @@
 	<view>
 		<!-- <text>helloworld</text> -->
 		<!-- <text>{{productList}}</text> -->
-		<view class="product-item">
+		<view class="product-box">
 			<view v-for="(item,index) in productList" :key="index" @click="goToDetail(item)">
 				<view class="product-image">
 					<image :src="item.coverImg" mode="aspectFill"></image>
 				</view>
-				<text class="product-name">{{ item.name }}</text>
-				<view class="product-info-box">
-					<text>{{ item.sellPrice / 100 }}</text>
+				<text class="product-name">
+					<h4>{{ item.name }}</h4>
+				</text>
+				<view class="product-info">
+					<text> {{ item.sellPrice / 100 }}</text>
 					<text>已售 {{ item.useStock }}</text>
 				</view>
 			</view>
 		</view>
 		<view>
-			<blockquote>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, quos.</blockquote>
+			<!-- <blockquote>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, quos.</blockquote> -->
 		</view>
 	</view>
 </template>
@@ -50,5 +52,26 @@
 </script>
 
 <style lang="scss">
+	.product-box {
+		margin: 80rpx;
 
+		image {
+			height: 600rpx;
+			width: 600rpx;
+		}
+
+		.product-name {
+			font-size: 50rpx;
+		}
+
+		.product-info {
+			text {
+				font-size: 30rpx;
+				padding: 0 30rpx;
+			}
+
+
+		}
+
+	}
 </style>
