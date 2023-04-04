d9922860468ff21b1e7eb2ce7b0bd3760686def6
zhengqingya <960869719@qq.com>
2023-04-04 15:08:47 +0800

    feat uniapp基础项目demo

---
+++ 知识库/前端/04-uniapp项目实战/my-base-app/pages/index/index.vue
@@ -0,0 +1,21 @@
+<template>
+	<view>
+		<text>xxx</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {};
+		},
+		onLoad() {},
+		methods: {
+
+		}
+	};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
