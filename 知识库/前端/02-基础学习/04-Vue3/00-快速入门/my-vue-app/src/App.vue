4f7c0d3f585eadd1055b7a13be8ee080346606a5
zhengqingya <960869719@qq.com>
2023-08-10 16:25:20 +0800

    docs note

--- 知识库/前端/02-基础学习/04-Vue3/00-快速入门/my-vue-app/src/App.vue
+++ 知识库/前端/02-基础学习/04-Vue3/00-快速入门/my-vue-app/src/App.vue
@@ -1,12 +1,17 @@
 <template>
-  <HelloWorld>
-    <template v-slot:left>具名插槽-left</template>
-    <template #right>具名插槽-right</template>
-  </HelloWorld>
+  <HelloWorld ref="helloRef" />
+  <button @click="handleClick">click</button>
+  <br />
+  <button @click="$refs.helloRef.changeData()">调用子组件方法</button>
 </template>
 
 <script setup>
+const { proxy } = getCurrentInstance();
 import HelloWorld from "./components/HelloWorld.vue";
+
+function handleClick() {
+  proxy.$refs.helloRef.changeData();
+}
 </script>
 
 <style scoped></style>
