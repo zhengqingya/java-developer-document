a80ea3a6059010ca39ac343daa8e52164af5fe69
zhengqingya <960869719@qq.com>
2024-06-10 20:39:49 +0800

    feat 摄像头打包名

--- 知识库/前端/08-Electron桌面应用/项目实战/摄像头/camera-app/src/renderer/src/stores/modules/settings.js
+++ 知识库/前端/08-Electron桌面应用/项目实战/摄像头/camera-app/src/renderer/src/stores/modules/settings.js
@@ -20,11 +20,11 @@ export const useSettingsStore = defineStore('settings', () => {
     if (config.value.isRound) {
       // 如果是圆角 设置窗口为1/1缩放
       window.api.setWindowSize({ AspectRatio: 1, width: 400, height: 400 })
-      ElMessage({ message: '圆角显示', type: 'success', duration: 500 })
+      // ElMessage({ message: '圆角显示', type: 'success', duration: 500 })
     } else {
       // 如果不是圆角 设置窗口为16/9缩放
       window.api.setWindowSize({ AspectRatio: 16 / 9, width: 500, height: 300 })
-      ElMessage({ message: '方框显示', type: 'success', duration: 500 })
+      // ElMessage({ message: '方框显示', type: 'success', duration: 500 })
     }
   }
 
